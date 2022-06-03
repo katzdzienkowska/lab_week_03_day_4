da36a14d09be9a1e45b903fe3007d9178f7f31bc
Kat Zdzienkowska <katzdzienkowska@kats-mbp.home>
2022-06-03 20:45:56 +0100

    reviewed

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,8 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app import app
-from models.events import event_list, add_new_event
-from models.event import Event
+from models.events import event_list, add_new_event, delete_event
+from models.event import *
+import datetime
 
 @app.route("/events")
 def index():
@@ -9,8 +10,9 @@ def index():
 
 @app.route("/events", methods=["post"])
 def add_event():
-    print(request.form)
     event_date = request.form["date"]
+    # split_date = date.split("-")
+    # date = datetime.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
     event_name = request.form["name"]
     event_guests = request.form["guests"]
     event_location = request.form["location"]
@@ -19,5 +21,10 @@ def add_event():
     # event_recurring = request.form["recurring"]
     new_event = Event(event_date, event_name, event_guests, event_location, event_description, event_recurring)
     add_new_event(new_event)
-    return render_template("index.html", title="Events", events=event_list)
+    return redirect("/events")
 
+@app.route('/events/delete/<name>', methods=['POST'])
+def delete(name):
+  delete_event(name)
+  return redirect('/events')
+  
--- models/events.py
+++ models/events.py
@@ -1,11 +1,20 @@
 from models.event import *
 
-event1 = Event("2022-06-01", "Bank Holiday", 10, "United Kingdom", "Bank holiday for Platinum Jubilee", False)
-event2 = Event("2022-06-01", "Class Lab", 2, "Room 6", "creating events page thing", True)
-event3 = Event("2022-06-01", "Monday Standup", 19, "Zoom classroom", "Morning standup", True)
+event1 = Event("2022, 06, 03", "Bank Holiday", 10, "United Kingdom", "Bank holiday for Platinum Jubilee", False)
+event2 = Event("2022, 06, 02", "Class Lab", 2, "Room 6", "creating events page thing", True)
+event3 = Event("2022, 06, 07", "Monday Standup", 19, "Zoom classroom", "Morning standup", True)
 
 event_list = [event1, event2, event3]
 
 def add_new_event(event):
     event_list.append(event)
+
+def delete_event(event_name):
+    event_to_delete = None
+    for event in event_list:
+        if event.name == event_name:
+            event_to_delete = event
+            break
+    
+    event_list.remove(event_to_delete)
 
